6e2633e443c9c69e3e864a41db6284f2f2e42a3a
brainox <macmartins081@gmail.com>
2021-10-09 14:17:48 +0100

    Initial Commit

---
+++ OnboardingViewsSwiftUI/OnboardingViewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewsSwiftUIApp.swift
+//  OnboardingViewsSwiftUI
+//
+//  Created by Decagon on 09/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingViewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
